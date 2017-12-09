20f88f5a6222d5c619086cd856fc2cb65129ab84
Daniel Misior <arreforall@gmail.com>
2017-12-09 18:16:57 +0100

    Added initial version of main game page

--- ThalassaApi/ThalassaApi.py
+++ ThalassaApi/ThalassaApi.py
@@ -4,18 +4,35 @@ from twisted.web.resource import Resource
 from twisted.internet import reactor
 
 class MainPageDispatcher(Resource):
-    isLeaf = True
-    def __init__(self):
-        super().__init__()
+
+    def getChild(self, name, request):
+        if name == b'':
+            return self
+        return Resource.getChild(self, name, request)
 
     def render_GET(self, request):
-        with open("/opt/Thalassa/html/login.html", 'rb') as file_stream:
+        with open("/opt/thalassa/ThalassaApi/html/login.html", 'rb') as file_stream:
+            data = file_stream.read()
+        request.setHeader(b"content-type", b"text/html")
+        return data
+
+class WorldDispatcher(Resource):
+    isLeaf = True
+
+    def getChild(self, name, request):
+        if name == '':
+            return self
+        return Resource.getChild(self, name, request)
+    
+    def render_GET(self, request):
+        with open("/opt/thalassa/ThalassaApi/html/world.html", 'rb') as file_stream:
             data = file_stream.read()
         request.setHeader(b"content-type", b"text/html")
         return data
 
 #resource = File('/tmp')
 root = MainPageDispatcher()
+root.putChild(b'world', WorldDispatcher())
 factory = Site(root)
 reactor.listenTCP(8888, factory)
 reactor.run()
